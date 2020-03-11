f759e2f00b074c2c907c55eb0f527f78cc7c94a9
Stephan Herbers <stephan.herbers@schuelerhilfe.de>
2020-03-11 15:12:38 +0100

    format with prettier

--- cache/index.js
+++ cache/index.js
@@ -1,134 +1,129 @@
-const BYPASS_COOKIES = ['DW', 'ttrss_sid']
+const BYPASS_COOKIES = ["DW", "ttrss_sid"];
 
-const BYPASS_URI = []
+const BYPASS_URI = [];
 
-const BYPASS_QUERY = []
+const BYPASS_QUERY = [];
 
-const CACHE_ON_STATUS = [200, 301, 302, 307, 404]
+const CACHE_ON_STATUS = [200, 301, 302, 307, 404];
 
 const TRACKING_QUERY = new RegExp(
-  '(gclid|utm_(source|campaign|medium)|fb(cl)?id|fbclid)',
-)
+  "(gclid|utm_(source|campaign|medium)|fb(cl)?id|fbclid)"
+);
 
-addEventListener('fetch', event => {
+addEventListener("fetch", event => {
   try {
-    let request = event.request
+    let request = event.request;
     // bypass cache on POST requests
-    if (request.method.toUpperCase() === 'POST') return
+    if (request.method.toUpperCase() === "POST") return;
     // bypass cache specific cookies, urls, or query parameter
-    if (checkBypassCache(request)) return
-    return event.respondWith(handleRequest(event))
+    if (checkBypassCache(request)) return;
+    return event.respondWith(handleRequest(event));
   } catch (err) {
-    return new Response(err.stack || err)
+    return new Response(err.stack || err);
   }
-})
+});
 
 async function handleRequest(event) {
   try {
-    let request = event.request
-    let cacheUrl = new URL(request.url)
-    cacheUrl = await removeCampaignQueries(cacheUrl)
-    let cacheRequest = new Request(cacheUrl, request)
-    let cache = caches.default
+    let request = event.request;
+    let cacheUrl = new URL(request.url);
+    cacheUrl = await removeCampaignQueries(cacheUrl);
+    let cacheRequest = new Request(cacheUrl, request);
+    let cache = caches.default;
 
     // Get response from origin and update the cache
-    let originResponse = getOrigin(
-      event,
-      request,
-      cache,
-      cacheRequest
-    )
-    event.waitUntil(originResponse)
+    let originResponse = getOrigin(event, request, cache, cacheRequest);
+    event.waitUntil(originResponse);
 
     // Use cache response when available, otherwise use origin response
-    let response = await cache.match(cacheRequest)
-    if (!response) response = await originResponse
+    let response = await cache.match(cacheRequest);
+    if (!response) response = await originResponse;
 
     // Send Logs to Elasticsearch
     event.waitUntil(logToES(request, response));
 
-    return response
+    return response;
   } catch (err) {
-    return new Response(err.stack || err)
+    return new Response(err.stack || err);
   }
 }
 
 async function getOrigin(event, request, cache, cacheRequest) {
   try {
     // Get response from orign
-    originResponse = await fetch(request)
+    originResponse = await fetch(request);
 
     // must use Response constructor to inherit all of response's fields
-    originResponse = new Response(originResponse.body, originResponse)
+    originResponse = new Response(originResponse.body, originResponse);
 
     if (CACHE_ON_STATUS.includes(originResponse.status)) {
       // Delete cookie header so HTML can be cached
-      originResponse.headers.delete('Set-Cookie')
+      originResponse.headers.delete("Set-Cookie");
 
       // waitUntil runs even after response has been sent
-      event.waitUntil(cache.put(cacheRequest, originResponse.clone()))
+      event.waitUntil(cache.put(cacheRequest, originResponse.clone()));
 
-      return originResponse
+      return originResponse;
     } else {
-      return originResponse
+      return originResponse;
     }
   } catch (err) {
-    return new Response(err.stack || err)
+    return new Response(err.stack || err);
   }
 }
 
 async function removeCampaignQueries(url) {
-  let deleteKeys = []
+  let deleteKeys = [];
 
   for (let key of url.searchParams.keys()) {
     if (key.match(TRACKING_QUERY)) {
-      deleteKeys.push(key)
+      deleteKeys.push(key);
     }
   }
 
-  deleteKeys.map(k => url.searchParams.delete(k))
+  deleteKeys.map(k => url.searchParams.delete(k));
 
-  return url
+  return url;
 }
 
 async function logToES(request, response) {
-  let ray = request.headers.get('cf-ray') || '';
+  let ray = request.headers.get("cf-ray") || "";
   let id = ray.slice(0, -4);
   let data = {
-    'timestamp': Date.now(),
-    'url': request.url,
-    'referer': request.referrer,
-    'method': request.method,
-    'ray': ray,
-    'ip': request.headers.get('cf-connecting-ip') || '',
-    'host': request.headers.get('host') || '',
-    'user-agent': request.headers.get('user-agent') || '',
-    'country': request.headers.get('Cf-Ipcountry') || '',
-    'status': response.status
+    timestamp: Date.now(),
+    url: request.url,
+    referer: request.referrer,
+    method: request.method,
+    ray: ray,
+    ip: request.headers.get("cf-connecting-ip") || "",
+    host: request.headers.get("host") || "",
+    "user-agent": request.headers.get("user-agent") || "",
+    country: request.headers.get("Cf-Ipcountry") || "",
+    status: response.status
   };
 
-  let url = "https://elasticsearch.sherbers.de/cloudflare/_doc/" + id
-  let auth = "Basic " + SECRET_ES_BASIC_AUTH
+  let url = "https://elasticsearch.sherbers.de/cloudflare/_doc/" + id;
+  let auth = "Basic " + SECRET_ES_BASIC_AUTH;
   await fetch(url, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify(data),
     headers: new Headers({
-      'Content-Type': 'application/json',
-      'Authorization': auth
+      "Content-Type": "application/json",
+      Authorization: auth
     })
-  })
+  });
 }
 
 function checkBypassCache(request) {
   try {
     if (BYPASS_COOKIES.length) {
-      const cookieHeader = request.headers.get('cookie')
+      const cookieHeader = request.headers.get("cookie");
       if (cookieHeader && cookieHeader.length) {
-        const cookies = cookieHeader.split(';')
+        const cookies = cookieHeader.split(";");
         for (let cookie of cookies) {
           for (let bypassCookie of BYPASS_COOKIES) {
             if (cookie.trim().startsWith(bypassCookie)) {
-              return true
+              return true;
             }
           }
         }
@@ -136,25 +131,25 @@ function checkBypassCache(request) {
     }
 
     if (BYPASS_URI.length) {
-      let url = new URL(request.url)
+      let url = new URL(request.url);
       for (let uri of BYPASS_URI) {
         if (url.pathname.includes(uri)) {
-          return true
+          return true;
         }
       }
     }
 
     if (BYPASS_QUERY.length) {
-      let url = new URL(request.url)
+      let url = new URL(request.url);
       for (let query of BYPASS_QUERY) {
         if (url.search.includes(query)) {
-          return true
+          return true;
         }
       }
     }
 
-    return false
+    return false;
   } catch (err) {
-    return new Response(err.stack || err)
+    return new Response(err.stack || err);
   }
 }
