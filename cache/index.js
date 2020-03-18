9922432a85c626ab6555ee5759f9f7d3a1f829cf
Stephan Herbers <stephan.herbers@schuelerhilfe.de>
2020-03-18 15:26:17 +0100

    revert logging for now, needs refactoring

--- cache/index.js
+++ cache/index.js
@@ -12,6 +12,11 @@ const TRACKING_QUERY = new RegExp(
 
 addEventListener("fetch", event => {
   try {
+    let request = event.request;
+    // bypass cache on POST requests
+    if (request.method.toUpperCase() === "POST") return;
+    // bypass cache specific cookies, urls, or query parameter
+    if (checkBypassCache(request)) return;
     return event.respondWith(handleRequest(event));
   } catch (err) {
     return new Response(err.stack || err);
@@ -26,51 +31,20 @@ async function handleRequest(event) {
     let cacheRequest = new Request(cacheUrl, request);
     let cache = caches.default;
 
-    // Get response from origin
-    let originResponse = getOrigin(request);
+    // Get response from origin and update the cache
+    let originResponse = getOrigin(
+      event,
+      request,
+      cache,
+      cacheRequest,
+      cacheUrl
+    );
     event.waitUntil(originResponse);
 
-    // bypass cache on POST requests and specific cookies, urls, or query parameter
-    if (request.method.toUpperCase() === "POST" || checkBypassCache(request)) {
-      let response = await originResponse;
-
-      // Send Logs to Elasticsearch
-      event.waitUntil(logToES(request, response));
-
-      return response;
-    } else {
-      // Use cache response when available, otherwise use origin response
-      let response = await cache.match(cacheRequest);
-      if (!response) response = await originResponse;
-
-      // cache response on certain status codes
-      if (CACHE_ON_STATUS.includes(originResponse.status)) {
-        // Delete cookie header so HTML can be cached
-        response.headers.delete("Set-Cookie");
-
-        // waitUntil runs even after response has been sent
-        event.waitUntil(cache.put(cacheRequest, response.clone()));
-      }
-
-      // Send Logs to Elasticsearch
-      event.waitUntil(logToES(request, response));
-
-      return response;
-    }
-  } catch (err) {
-    return new Response(err.stack || err);
-  }
-}
-
-async function getOrigin(request) {
-  try {
-    // Get response from orign
-    originResponse = await fetch(request);
-
-    // must use Response constructor to inherit all of response's fields
-    originResponse = new Response(originResponse.body, originResponse);
-
-    return originResponse;
+    let response = await cache.match(cacheRequest);
+    // Use cache response when available, otherwise use origin response
+    if (!response) response = await originResponse;
+    return response;
   } catch (err) {
     return new Response(err.stack || err);
   }
@@ -79,7 +53,7 @@ async function getOrigin(request) {
 async function removeCampaignQueries(url) {
   let deleteKeys = [];
 
-  for (let key of url.searchParams.keys()) {
+  for (var key of url.searchParams.keys()) {
     if (key.match(TRACKING_QUERY)) {
       deleteKeys.push(key);
     }
@@ -90,32 +64,28 @@ async function removeCampaignQueries(url) {
   return url;
 }
 
-async function logToES(request, response) {
-  let ray = request.headers.get("cf-ray") || "";
-  let id = ray.slice(0, -4);
-  let data = {
-    timestamp: Date.now(),
-    url: request.url,
-    referer: request.referrer,
-    method: request.method,
-    ray: ray,
-    ip: request.headers.get("cf-connecting-ip") || "",
-    host: request.headers.get("host") || "",
-    "user-agent": request.headers.get("user-agent") || "",
-    country: request.headers.get("Cf-Ipcountry") || "",
-    status: response.status
-  };
+async function getOrigin(event, request, cache, cacheRequest, cacheUrl) {
+  try {
+    // Get response from orign
+    originResponse = await fetch(request);
 
-  let url = "https://elasticsearch.sherbers.de/cloudflare/_doc/" + id;
-  let auth = "Basic " + SECRET_ES_BASIC_AUTH;
-  await fetch(url, {
-    method: "PUT",
-    body: JSON.stringify(data),
-    headers: new Headers({
-      "Content-Type": "application/json",
-      Authorization: auth
-    })
-  });
+    // must use Response constructor to inherit all of response's fields
+    originResponse = new Response(originResponse.body, originResponse);
+
+    if (CACHE_ON_STATUS.includes(originResponse.status)) {
+      // Delete cookie header so HTML can be cached
+      originResponse.headers.delete("Set-Cookie");
+
+      // waitUntil runs even after response has been sent
+      event.waitUntil(cache.put(cacheRequest, originResponse.clone()));
+
+      return originResponse;
+    } else {
+      return originResponse;
+    }
+  } catch (err) {
+    return new Response(err.stack || err);
+  }
 }
 
 function checkBypassCache(request) {
